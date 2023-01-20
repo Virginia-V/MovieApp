96732373aa2b748b6208e4644e1f67331b070349
Virginia Varzari <varzarivirginia4@gmail.com>
2023-01-20 18:39:52 +0200

    Add the Filter Component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
+import Filter from "./Filter";
 
 function App() {
   const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
 
   const fetchPopular = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=8a60434cec2287e948191fda92048912&language=en-US&page=1"
     );
     const movies = await data.json();
-    console.log(movies);
     setPopular(movies.results);
+    setFiltered(movies.results);
   };
 
   useEffect(() => {
@@ -20,6 +23,12 @@ function App() {
 
   return (
     <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
       <div className="popular-movies">
         {popular.map((movie) => {
           return <Movie key={movie.id} movie={movie} />;
